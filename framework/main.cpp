6251b6c2147f592a42f4011da4f69c022f97bfbb
LH <huanliu_uestc@163.com>
2024-10-30 15:40:00 +0800

    dcu

--- framework/main.cpp
+++ framework/main.cpp
@@ -6,13 +6,13 @@
 #include <Kokkos_Core.hpp>
 #endif // VTKM_HIP
 
+int main(int argc, char* argv[])
+{
 #ifdef VTKM_HIP
 #include <Kokkos_Core.hpp>
 #endif // VTKM_HIP
 
 
-int main(int argc, char* argv[])
-{
   std::shared_ptr<Application> app = std::make_shared<MDApplication>(argc, argv);
   app->Run();
 
