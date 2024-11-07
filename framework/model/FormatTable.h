6bf24cf96f9c45de387b8629c89fa0bc18f3103b
LH <huanliu_uestc@163.com>
2024-11-07 15:20:44 +0800

    order

--- framework/model/FormatTable.h
+++ framework/model/FormatTable.h
@@ -25,28 +25,21 @@
 //==================================================================================
 
 #pragma once
-#include "TableBase.h"
-#include <fstream>
-#include <iostream>
-class FormatTable : public TableBase
+
+#include <unordered_map>
+#include <vector>
+#include <string>
+class TableBase
 {
 public:
-  FormatTable();
-  virtual ~FormatTable();
+	TableBase() = default;
+	virtual ~TableBase() = default;
 
-  bool AddHeader(const std::string& name) override;
-  bool AddData(const std::string& name, const std::string& data) override;
-  void Print() override;
-  bool IsEmpty();
-  void LogFile();
+	virtual bool AddHeader(const std::string& name) = 0;
+	virtual bool AddData(const std::string& name, const std::string& data) = 0;
+	virtual void Print() = 0;
 
 protected:
-  void PrintHeader();
-  void PrintRowData();
-
-protected:
-  const unsigned int _header_spacing; // use to print header
-  const unsigned int _column_width;
-  long _row_count;
-  std::ofstream _log_file;
+	std::unordered_map<std::string, std::string> _row_data;
+	std::vector<std::string> _keys;
 };
